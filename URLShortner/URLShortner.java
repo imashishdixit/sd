a57bdaaa36cc8d7675c8f5c215cb1ac128c5e5de
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2026-01-13 10:37:42 +0000

    general

---
+++ URLShortner/URLShortner.java
@@ -0,0 +1,15 @@
+class URLShortner{
+    private int a;
+    private String name;
+    public URLShortner(int a, String name){
+        this.a=a;
+        this.name=name;
+    }
+    public String getNameS(){
+        return this.name;
+    }
+    public int geta(){
+        return this.a;
+    }
+
+}
---
+++ URLShortner/main.java
@@ -0,0 +1,7 @@
+class Main{
+    public static void main(String [] args){
+        URLShortner obj = new URLShortner(10,"ashish");
+        System.out.println(obj.getNameS());
+         System.out.println(obj.geta());
+    }
+}
